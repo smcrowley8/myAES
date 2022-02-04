320504abb99da88db615deb515197fdc94a4cb64
smcrowley8 <smcrowley8@gmail.com>
2022-02-04 14:47:54 -0500

    changes

--- tests/test_backend.py
+++
@@ -1,7 +0,0 @@
-import pytest 
-from myAES import __version__
-
-
-
-def test_version():
-    assert __version__ == "0.1.0"
---
+++ tests/test_unit.py
@@ -0,0 +1,18 @@
+import pytest 
+from myAES import __version__
+from myAES.BinaryGaloisFeild import GF2
+from myAES.AES import AES
+
+from rich.console import Console 
+
+console = Console() 
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+def test_GF2_polynomial():
+    """Test if polynomial output matches expected"""
+    expectedPolyStr = "x "
+    testGF = GF2(2)
+    testPolynomialString = testGF.toPolynomial()
+    assert test_GF2_polynomial == expectedPolyStr, f"{expectedPolyStr=}, {testPolynomialString=}"
